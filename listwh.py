f5c0a059ff7bbb96af47bf60640144d04af644e0
“yvonne” <3232754308@qq.com>
2023-01-16 11:08:34 +0800

    if while

---
+++ listwh.py
@@ -0,0 +1,31 @@
+#列表(可以包括任意类型的数据，但是一般不建议)
+nums = [11,22,33,44,55,66,77,88,99]
+names =["yvonne","lily","lucy"]
+
+# print(names)
+# print(names[0])
+
+# print(names[3])
+#越界，程序会报错
+
+#print(names[0:2]) #2不包含
+
+#遍历：依次取出列表中的每一个元素
+
+# #while
+# i=0
+# while i<len(names): #len()获取列表的长度
+#     print(names[i])
+#     i+=1
+
+# # for(不需要知道列表的长度)
+# for name in names:
+#     print(name)
+
+#列表的增删改查
+
+#增加
+#names.append("chenxin") #在列表的最后添加一个元素
+print(names)
+
+
