2c78f10f06811094d5094824fc1722cfa2fe4c7d
“yvonne” <3232754308@qq.com>
2023-02-27 16:03:03 +0800

    Ture False 比较运算符 逻辑运算符

--- TFsign-4.py
+++ TFsign-4.py
@@ -20,31 +20,34 @@ print(1 == 1) #True
 print(1 != 1) #False
 print(100 > 1)
 print(100 < 1)
-print(1 >= 1)#True
+print(1 >= 1)#True 理解为大于或者等于
 print(1 <= 1)
 
 print("你的年龄是")
 age = input()#  age = input("你的年龄是")
 if int(age) >= 18: # '>=' not supported 'str' and 'int'
+    # 如果直接写age>=18，会报错，因为input()输入的是字符串，而18是整数，两者不能比较
+    #int()函数可以将字符串转换为整数,前提是数字
     print("你可以看")
 else:
     print("你不能看")
 
 #逻辑运算符
-#and
+#and 与
 print(True and False)#False
 
-#or
+#or 或
 print(True or False)#True
 
-#not
+#not 非
 print(not (100>2))#False
 
-#demo1:用户登录
+#demo1:用户登录 and
 name = input("请输入用户名：")
 password = input("请输入密码：")
 print("能登陆吗？")
 print(name == "admin" and password == "123456")
+#默认用户是admin，密码是123456 最后一句输出True/False
 
 #demo2:开门
 role = input("请输入角色：")
@@ -54,16 +57,21 @@ print(role == "admin" or role == "manager")
 #demo3:判断是否是18~50之间的女性，然后，判断不在这个范围之间的人
 gender = "女"
 age = 18
+
 # print("范围内的女性吗？")
 # print(age>=18 and age<=50 and gender=="女")
+
 print("范围外的人吗？")
 print(not(age>=18 and age<=50 and gender=="女"))
 
 #特殊情况
 print(100 and 200)#200(最后的结果)
 #C语言中，0为假，非0为真，python中，0为False，非0为True
-print(0 and 200)#0(是False就不再执行右侧)--熔断
+#相当于True and True，and左右两边没有Ture和False，所以取最后一个值返回
+#200不是0，所以是True
 
-print(100 or 200)#100--熔断
+print(0 and 200)#0(and中是False就不再执行右侧)--熔断
+
+print(100 or 200)#100--熔断(or中是True就不再执行右侧)
 print(0 or 200)#200
 
